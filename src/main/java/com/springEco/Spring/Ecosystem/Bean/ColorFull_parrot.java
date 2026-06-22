adcab087209fc566b6781fe41d62532692bdef61
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-22 14:35:19 +0900

    added only beans whose color is green

---
+++ src/main/java/com/springEco/Spring/Ecosystem/Bean/ColorFull_parrot.java
@@ -0,0 +1,27 @@
+package com.springEco.Spring.Ecosystem.Bean;
+
+public class ColorFull_parrot {
+    String name;
+    String color;
+
+    public ColorFull_parrot(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/springEco/Spring/Ecosystem/SpringEcosystemApplication.java
+++ src/main/java/com/springEco/Spring/Ecosystem/SpringEcosystemApplication.java
@@ -1,6 +1,7 @@
 package com.springEco.Spring.Ecosystem;
 
 import com.springEco.Spring.Ecosystem.Bean.Cat;
+import com.springEco.Spring.Ecosystem.Bean.ColorFull_parrot;
 import com.springEco.Spring.Ecosystem.Bean.Parrot;
 import com.springEco.Spring.Ecosystem.Config.Config;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Primary;
 
+import java.util.ArrayList;
 import java.util.function.Supplier;
 
 @SpringBootApplication
@@ -35,11 +37,33 @@ public class SpringEcosystemApplication {
 		var parrotGreen=contex.getBean("parrot_green1",Parrot.class);
 		System.out.println(parrotGreen.getName());
 
+		ArrayList<ColorFull_parrot> parrots = getColorFullParrots();
+
+		for(ColorFull_parrot c: parrots){
+			if(c.getColor().equals("green")){
+				Supplier<ColorFull_parrot>supplier=()->c;
+				contex.registerBean(c.getName(), ColorFull_parrot.class,supplier);
+				System.out.println("added");
+			}
+		}
 
 
 	}
 
+	private static ArrayList<ColorFull_parrot> getColorFullParrots() {
+		ArrayList<ColorFull_parrot>parrots=new ArrayList<>();
 
+		ColorFull_parrot colorFull_parrot1= new ColorFull_parrot("parrotbek","green");
+		ColorFull_parrot colorFull_parrot2= new ColorFull_parrot("parrotbek-2","red");
+		ColorFull_parrot colorFull_parrot3= new ColorFull_parrot("parrotbek-3","yellow");
+		ColorFull_parrot colorFull_parrot4= new ColorFull_parrot("parrotbek-4","green");
+
+		parrots.add(colorFull_parrot1);
+		parrots.add(colorFull_parrot2);
+		parrots.add(colorFull_parrot3);
+		parrots.add(colorFull_parrot4);
+		return parrots;
+	}
 
 
 }
