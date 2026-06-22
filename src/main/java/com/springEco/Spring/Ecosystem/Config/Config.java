c523ba5cf002c10d5e2ead49d38a03637f94be8e
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-22 14:01:48 +0900

    used @Primary annotation

--- src/main/java/com/springEco/Spring/Ecosystem/Config/Config.java
+++ src/main/java/com/springEco/Spring/Ecosystem/Config/Config.java
@@ -3,6 +3,7 @@ package com.springEco.Spring.Ecosystem.Config;
 import com.springEco.Spring.Ecosystem.Bean.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class Config {
@@ -13,4 +14,21 @@ public class Config {
         par1.setName("kiki");
         return par1;
     }
+
+    @Bean
+    public Parrot parrot2(){
+        Parrot parrot=new Parrot();
+        parrot.setName("parrot-2");
+        return parrot;
+    }
+
+    @Bean
+    @Primary
+    public Parrot parrot3(){
+        Parrot parrot=new Parrot();
+        parrot.setName("parrot-3");
+        return parrot;
+    }
+
+
 }
