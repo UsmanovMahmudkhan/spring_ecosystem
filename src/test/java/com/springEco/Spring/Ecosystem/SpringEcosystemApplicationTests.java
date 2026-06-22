bf17042a0c22d15c473d3664b3f6691e99f8150e
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-22 13:40:49 +0900

    Initial Spring ecosystem project

---
+++ src/test/java/com/springEco/Spring/Ecosystem/SpringEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springEco.Spring.Ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
