14f06f88adb2ac74068404020156aa64d98ae093
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-06-28 20:50:09 +0900

    added structure of project

---
+++ Mini-Task-Tracker-project/src/main/java/com/task_tracker/Mini/Task/Tracker/project/MiniTaskTrackerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.task_tracker.Mini.Task.Tracker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTaskTrackerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTaskTrackerProjectApplication.class, args);
+	}
+
+}
---
+++ Mini-Task-Tracker-project/src/test/java/com/task_tracker/Mini/Task/Tracker/project/MiniTaskTrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_tracker.Mini.Task.Tracker.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTaskTrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
