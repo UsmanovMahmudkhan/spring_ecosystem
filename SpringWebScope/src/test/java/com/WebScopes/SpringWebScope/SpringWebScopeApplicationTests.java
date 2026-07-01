cbcaedc84e0cf2e0e7911323e6996300f3fbd30f
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-07-01 13:14:50 +0900

    upload dependencies

---
+++ SpringWebScope/src/test/java/com/WebScopes/SpringWebScope/SpringWebScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebScopes.SpringWebScope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
