fbd51b4daf5af4f8b22b6a004c56b6d747c805e3
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-07-12 13:46:34 +0900

    used row jdbc connection
    # Conflicts:
    #	.gitignore

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+class Main{
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/university";
+        String username="root";
+        String password="test123";
+        String sql="SELECT * FROM university.courses";
+        try {
+            Connection connection= DriverManager.getConnection(url,username,password);
+            PreparedStatement statement=connection.prepareStatement(sql);
+            ResultSet set=statement.executeQuery();
+            while(set.next()){
+                System.out.println(set.getString("title"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
