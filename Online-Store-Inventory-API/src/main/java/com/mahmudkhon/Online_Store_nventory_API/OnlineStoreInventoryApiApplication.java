f62e7b5119c43efda936549dd1972590f66f74f2
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-07-19 20:12:58 +0900

    add template

---
+++ Online-Store-Inventory-API/src/main/java/com/mahmudkhon/Online_Store_nventory_API/OnlineStoreInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.mahmudkhon.Online_Store_nventory_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreInventoryApiApplication.class, args);
+	}
+
+}
---
+++ Online-Store-Inventory-API/src/test/java/com/mahmudkhon/Online_Store_nventory_API/OnlineStoreInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmudkhon.Online_Store_nventory_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
